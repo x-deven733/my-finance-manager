7c1f92ab785c88e7ad4b74b16865479f29ee0b7b
Devendra Mishra <mdevendra337@gmail.com>
2019-09-26 01:39:07 +0530

    Updates

---
+++ precache-manifest.747549e35d6899ceb1d66c7a58371afc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b436412662402c70a439b05e3d330bf8",
+    "url": "/my-finance-manager/index.html"
+  },
+  {
+    "revision": "8e93ff833dcf8b092267",
+    "url": "/my-finance-manager/static/css/main.46e9ec59.chunk.css"
+  },
+  {
+    "revision": "860f1505d715b74a0f00",
+    "url": "/my-finance-manager/static/js/2.782a38bf.chunk.js"
+  },
+  {
+    "revision": "8e93ff833dcf8b092267",
+    "url": "/my-finance-manager/static/js/main.3379fb0e.chunk.js"
+  },
+  {
+    "revision": "ec48a22de85a4492e95d",
+    "url": "/my-finance-manager/static/js/runtime-main.c9ace5d2.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/my-finance-manager/precache-manifest.747549e35d6899ceb1d66c7a58371afc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-finance-manager/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
